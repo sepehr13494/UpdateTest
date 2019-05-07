a3da0160d2108c0c237f34c1c4b946d1e384b2d4
Sepehr <marash.sepehr@gmail.com>
2019-05-07 16:01:13 +0430

    adaq layouts

--- app/src/main/java/ir/boardbazi/updatetest/MainActivity.java
+++ app/src/main/java/ir/boardbazi/updatetest/MainActivity.java
@@ -1,7 +1,12 @@
 package ir.boardbazi.updatetest;
 
+import android.support.constraint.Guideline;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import retrofit2.Call;
@@ -10,10 +15,15 @@ import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    Guideline guideline;
+    RelativeLayout layout;
+    ImageView menu_img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_talk_show);
+
+        menu_img = findViewById(R.id.menu_img);
 
 
         APIInterface apiInterface = APIClient.getClient().create(APIInterface.class);
@@ -30,5 +40,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        guideline = findViewById(R.id.guideline3);
+        layout = findViewById(R.id.layout);
+        guideline.setGuidelinePercent(0.4f);
+        LayoutInflater.from(this).inflate(R.layout.test,layout);
+
+        menu_img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeAllViews();
+                guideline.setGuidelinePercent(0.55f);
+                LayoutInflater.from(MainActivity.this).inflate(R.layout.comments,layout);
+            }
+        });
+
+
+
     }
 }
